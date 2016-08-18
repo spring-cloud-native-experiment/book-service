18acdb4ca615bb26e0ba5d37f13dbe96ddc3cf07
Praveer Gupta <praveerg@inpraveerg.local>
2016-08-18 12:08:03 +0530

    Allow running tests in the absence of discovery server.
    Test can be run by calling `SPRING_CLOUD_BOOTSTRAP_ENABLED=false ./gradlew clean test`
    
    http://stackoverflow.com/questions/34307529/strategy-for-unit-testing-a-spring-cloud-service

--- src/main/java/com/example/book/BookServiceApplication.java
+++ src/main/java/com/example/book/BookServiceApplication.java
@@ -2,13 +2,19 @@ package com.example.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
     }
+
+    @Configuration
+    @EnableEurekaClient
+    @ConditionalOnProperty("spring.cloud.bootstrap.enabled")
+    static class CloudConfiguration {}
 }
